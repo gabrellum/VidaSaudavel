b872d4023179baf12dadb1f35dbc579b66c10fa6
gabrellum <gabrielly.0908@aluno.pr.senac.br>
2024-03-06 15:25:02 -0300

    alteração cliente database

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,13 @@ require('dotenv/config')
 module.exports = {
 
     development: {
-        client: 'mysql2',
+        client: 'pg',
         connection: {
           host: process.env.HOST,
           user: process.env.USER,
           password: process.env.PASSWORD,
-          database: process.env.DATABASE
-          
+          database: process.env.DATABASE,
+          port: process.env.DB_PORT,
         },
        
         migrations: { 
