b84fedcd587d8920a58b42da6de947de9ed52ffa
gabrellum <gabrielly.0908@aluno.pr.senac.br>
2024-03-07 14:37:34 -0300

    tirei um couchete

--- src/database/knex/seeds/createAdmin.js
+++ src/database/knex/seeds/createAdmin.js
@@ -1,12 +1,12 @@
 require('dotenv/config')
 exports.seed = async (knex) => {
 
-  await knex('users').insert([
+  await knex('users').insert(
     { 
       email: process.env.ADMIN_EMAIL,
       isAdmin: true,
       name: process.env.USER_ADMIN,
       password: process.env.ADMIN_PASSWORD,
     },
-  ]);
+  );
 };
