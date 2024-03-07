8318a36edd4333740d7e427b915136d1fb20eff3
gabrellum <gabrielly.0908@aluno.pr.senac.br>
2024-03-07 14:36:11 -0300

    alteração na migration User

--- src/database/knex/migrations/20240306190939_createUsers.js
+++ src/database/knex/migrations/20240306190939_createUsers.js
@@ -1,10 +1,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable("users", (table) => {
         table.increments("id").primary();
-        table.string("name").notNullable();
         table.string("email").notNullable();
-        table.string("password").notNullable();
         table.boolean("isAdmin").defaultTo("false");
+        table.string("name").notNullable();
+        table.string("password").notNullable();
 
         table.timestamp("created_at").defaultTo(knex.fn.now());
         table.timestamp("updated_at").defaultTo(knex.fn.now());
