575b66bba2006ed7936cb3f3ec8713506322d50b
gabrellum <gabrielly.0908@aluno.pr.senac.br>
2024-03-07 14:21:34 -0300

    tentando arrumar

--- src/database/knex/seeds/createAdmin.js
+++ src/database/knex/seeds/createAdmin.js
@@ -3,10 +3,10 @@ exports.seed = async (knex) => {
 
   await knex('users').insert([
     { 
-      name: process.env.USER_ADMIN,
       email: process.env.ADMIN_EMAIL,
+      isAdmin: true,
+      name: process.env.USER_ADMIN,
       password: process.env.ADMIN_PASSWORD,
-      isAdmin: true
     },
   ]);
 };
