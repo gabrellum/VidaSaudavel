d1942780e49e90bd28e7ea68fa78c11929512ea6
gabrellum <gabrielly.0908@aluno.pr.senac.br>
2024-03-07 14:04:47 -0300

    arrumei o erro(boolean)

--- src/database/knex/migrations/20240306190939_createUsers.js
+++ src/database/knex/migrations/20240306190939_createUsers.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
         table.string("name").notNullable();
         table.string("email").notNullable();
         table.string("mensagem").notNullable();
-        table.bolean("isAdmin").defaultTo("false");
+        table.boolean("isAdmin").defaultTo("false");
 
         table.timestamp("created_at").defaultTo(knex.fn.now());
         table.timestamp("updated_at").defaultTo(knex.fn.now());
