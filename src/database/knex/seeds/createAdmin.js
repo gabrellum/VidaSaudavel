71fa84b8d14cb10abda98a9bc1c57cf5cde877a2
gabrellum <gabrielly.0908@aluno.pr.senac.br>
2024-03-07 14:07:39 -0300

    arrumei erro denovo (isAdmin)

--- src/database/knex/seeds/createAdmin.js
+++ src/database/knex/seeds/createAdmin.js
@@ -6,7 +6,7 @@ exports.seed = async (knex) => {
       name: process.env.USER_ADMIN,
       email: process.env.ADMIN_EMAIL,
       password: process.env.ADMIN_PASSWORD,
-      IsAdmin: true
+      isAdmin: true
     },
   ]);
 };
