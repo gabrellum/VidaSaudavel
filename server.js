e3b749c2e5e42ac3dcd2fcf0d7b9a2b4821ea2b9
gabrellum <gabrielly.0908@aluno.pr.senac.br>
2024-03-08 15:07:57 -0300

    Ajuste Cors

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const express = require('express')
 const routes = require('./src/routes/')
 
 const app = express()
+app.use(cors())
+
 app.use(express.json())
 
-app.use(cors())
 
 /* migrationRun()  */
 
